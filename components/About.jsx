3d98f636c242d96b160445dad04ce7f1ab56fd81
Haohao Yu <haohao@kth.se>
2024-05-26 19:58:30 +0200

    added About modal

---
+++ components/About.jsx
@@ -0,0 +1,25 @@
+const About = () => {
+    return (
+      <div>
+        <h2 className="text-xl font-bold mb-4">About Information</h2>
+        <p>
+          Demo video
+          <br />
+          Team
+          <br />
+          project description (1 points)
+          <br />
+          The initial idea behind this project is that i think serious music nerds should not only rely on algorithm recommendation, instead explore music based on your favorite artists. A good hint of this method in music is song covering—a cover is a tribute. Want to know who are influenced by your favorite artists and your fav artists are influenced by whom? Go to find all the cover data of them!
+          <br />
+          secondhandsongs.com is a good place for this, however it doesn’t provide a good GUI interface. Therefore, this project is to help.
+          <br />
+          learning objectives reached (1 points)
+          <br />
+          citations, references, source of code and data
+        </p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
